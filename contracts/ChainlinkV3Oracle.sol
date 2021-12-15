ff4af8393016d3a4700063466fb2bcc870c66856
Walodja1987 <wladimir.weinbender@gmail.com>
2021-12-15 11:47:06 +0100

    test cases extended
    * Test file split into 2 to have a separate file for each contract
    * Test cases extended, in particular, setFinalReferenceValue tests introduced
    * Clean-up
    * IMPORTANT: You have to comment out the two require statments in lines 29 & 30 in ChainlinkV3Oracle.sol in order for the setFinalReferenceValue tests to work. This is because there is no Graph for Chainlink on Kovan and hence I am not able to automatically find out the roundId for a given expiry timestamp. I will try to find a more elegant solution for that problem later

--- contracts/ChainlinkV3Oracle.sol
+++ contracts/ChainlinkV3Oracle.sol
@@ -4,11 +4,11 @@ pragma solidity 0.8.4;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./interfaces/IDIVA.sol";
 
-import "hardhat/console.sol";
+// IMPORTANT: Activate the two require statements in setFinalReferenceValueById if you have deactivated them for testing!
 
 contract ChainlinkV3Oracle {
 
-    event SetFinalReferenceValue(uint256 indexed optionID, uint256 decimalAdjustedHistoricalPrice, uint256 indexed expiryDate, uint256 indexed roundid );
+    event SetFinalReferenceValue(uint256 indexed pooId, uint256 decimalAdjustedHistoricalPrice, uint256 indexed expiryDate, uint256 indexed roundid );
     
     address private _chainlinkAddress;
 
@@ -17,35 +17,26 @@ contract ChainlinkV3Oracle {
     }
 
     // Conscious decision to have the addressDIVAFactory as input in setFinalPriceByID function to avoid re-deploying
-    function setFinalReferenceValue(address _addressDIVAFactory, uint80 _roundId, uint256 _optionId) public returns (bool) {
+    function setFinalReferenceValue(address _addressDIVAFactory, uint80 _roundId, uint256 _pooId) public returns (bool) {
         IDIVA _DIVAFactory = IDIVA(_addressDIVAFactory);
 
-        //Fetching option expiry date so that one cannot call maliciously call this function with wrong roundId
-        // _DIVAFactory.Pool storage params = _DIVAFactory.getPoolParametersById(_optionId);
-        IDIVA.Pool memory params = _DIVAFactory.getPoolParametersById(_optionId);
+        IDIVA.Pool memory params = _DIVAFactory.getPoolParametersById(_pooId);
 
         uint256 expiryDate = params.expiryDate;
 
         (uint80 returnedRoundId, uint256 historicalPrice, uint256 roundIdStartedAt, uint256 roundIdTimestamp, uint80 answeredInRound, uint8 decimals) = getHistoricalPrice(_roundId); //TODO: Consider adding timestamp check--DONE
-        
 
-        require((roundIdStartedAt <= expiryDate) && (expiryDate > roundIdTimestamp) , "Option expiry date does not match with roundId timestamp"); // Checking expiry date within 60 second window
-        require((returnedRoundId == answeredInRound) , "Chainlink price based on date is answered in different roundID");
-
-        //TODO input price needs to be 18 digits
-        //historical price adjusted for historicalprice*10**(18-decimal)
-        //set price withing the diva factory contract. permission checks/ expiry checks etc are handeled by diva factory
-
+        require((roundIdStartedAt <= expiryDate) && (expiryDate > roundIdTimestamp) , "ChainlinkV3Oracle: expiry date outside of round"); // Checking expiry date within 60 second window
+        require(returnedRoundId == answeredInRound , "ChainlinkV3Oracle: round not equal to answered round");
         uint256 decimalAdjustedHistoricalPrice = historicalPrice * (10**(18-decimals));
-        require(_DIVAFactory.setFinalReferenceValueById(_optionId, decimalAdjustedHistoricalPrice, false)); //passing on to diva, ultimate handover. Retain false bool. 
-        emit SetFinalReferenceValue(_optionId, decimalAdjustedHistoricalPrice, expiryDate, _roundId);
+        require(_DIVAFactory.setFinalReferenceValueById(_pooId, decimalAdjustedHistoricalPrice, false)); //passing on to diva, ultimate handover. Retain false bool. 
+        emit SetFinalReferenceValue(_pooId, decimalAdjustedHistoricalPrice, expiryDate, _roundId);
         return true;
     }
 
     function getChainlinkOracleAddress() public view returns (address) {
         return _chainlinkAddress;
     }
-
 
     function getHistoricalPrice(uint80 _roundId) public view returns (uint80, uint256, uint256, uint256, uint80, uint8) {
 
@@ -57,13 +48,11 @@ contract ChainlinkV3Oracle {
             uint256 timeStamp,
             uint80 answeredInRound
         ) = chainlinkOracleContract.getRoundData(_roundId);
-        require(timeStamp > 0, "Chainlink: Round not complete");
-        require(price >= 0, "Chainlink: Negative price");
+        require(timeStamp > 0, "Chainlink: round not complete");
+        require(price >= 0, "Chainlink: negative price");
         uint8 decimals = chainlinkOracleContract.decimals();
+        require(decimals <= 18, "Chainlink: exceeds max allowed decimals");
         return (id, uint256(price), startedAt, timeStamp, answeredInRound, decimals);
     }
-
-   
-
 
 }
---
+++ test/ChainlinkV3Oracle.test.js
@@ -0,0 +1,171 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { chainlinkV3OracleAttachFixture } = require("./fixtures/ChainlinkV3OracleFixture")
+const { chainlinkV3OracleFactoryDeployFixture } = require("./fixtures/ChainlinkV3OracleFactoryFixture")
+const { erc20DeployFixture, erc20AttachFixture } = require("./fixtures/ERC20TokenFixture")
+const { BigNumber, providers } = require('ethers')
+const { parseEther } = require('@ethersproject/units')
+
+const { abi: Pool_ABI } = require('../contracts/abi/PoolFacet.json');
+const { abi: Getter_ABI } = require('../contracts/abi/GetterFacet.json');
+
+describe('ChainlinkV3Oracle', () => {
+  let chainlinkV3OracleFactory;
+  let addresses;
+  let chainlinkAddress;
+  let poolFacet;
+  let divaKovanAddress;
+  let deployer;
+  let user1;
+  let user2;
+  let oracle;
+  let getExpiryInSeconds;
+
+
+  getExpiryInSeconds = (offsetInSeconds) =>
+        Math.floor(Date.now() / 1000 + offsetInSeconds).toString(); // 60*60 = 1h; 60*60*24 = 1d, 60*60*24*365 = 1y
+
+  // getPastExpiry = (secondsAgo) => Math.floor(Date.now() / 1000 - secondsAgo).toString();
+
+  beforeEach(async () => {
+      [deployer, user1, user2, oracle] = await ethers.getSigners();
+      chainlinkV3OracleFactory = await chainlinkV3OracleFactoryDeployFixture();
+      chainlinkAddress = '0x9326BFA02ADD2366b30bacB125260Af641031331'; // oracle address for ETH/USD on Kovan
+      divaKovanAddress = '0x93640bd8fEa53919A102ad2EEA4c503E640eDDAd';
+
+      const txCreateOracle = await chainlinkV3OracleFactory.createChainlinkV3Oracle(chainlinkAddress);
+      await txCreateOracle.wait();
+      addresses = await chainlinkV3OracleFactory.getChainlinkV3Oracles();
+  });
+  
+  describe('getChainlinkOracleAddress', async () => {
+    it('Should return the oracle address of the oracle instance', async () => {
+      const oracleContract = await chainlinkV3OracleAttachFixture(addresses[0])
+      const oracelAddresses = await oracleContract.getChainlinkOracleAddress();
+      expect(oracelAddresses).to.eq(chainlinkAddress)
+    })
+  })
+  
+  describe('getHistoricalPrice', async () => {
+    it('Should return the price for a given round Id', async () => {
+      const oracleContract = await chainlinkV3OracleAttachFixture(addresses[0])
+      round_id = '36893488147419112854'
+      price = 412900500000
+      
+      const historicalPrice = await oracleContract.getHistoricalPrice(round_id);
+      expect(historicalPrice[1]).to.eq(price);
+    })
+  })
+
+  describe('setFinalReferenceValueById', async () => {
+    // IMPORTANT: To test this, you have to remove the two require statements in the setFinalReferenceValueById part
+    let erc20;
+    let chainlinkOracle;
+
+    beforeEach(async () => {
+      poolFacet = await ethers.getContractAt(Pool_ABI, divaKovanAddress);
+      getterFacet = await ethers.getContractAt(Getter_ABI, divaKovanAddress)
+      erc20 = await erc20DeployFixture("DummyToken", "DCT", "1000000000000000000000000"); // 1mio tokens
+      const approveTx = await erc20.approve(poolFacet.address, "1000000000000000000000001");
+      await approveTx.wait();
+      chainlinkOracle = await chainlinkV3OracleAttachFixture(addresses[0])
+    })
+
+    it('Should set final reference value equal to inflection when triggered after submission period and no input provided', async () => {
+      // QUESTION: Should it set the value to inflection? I thought that this happens at first redemption?
+      let tx = await poolFacet.createContingentPool(
+        [
+          parseEther("17"), // inflection
+          parseEther("33"), // cap
+          parseEther("10"), // floor
+          parseEther("100"), // collateral balance short
+          parseEther("100"), // collateral balance long
+          1634864960, // expiry date (long expired; corresponds to roundId = '36893488147419112854')
+          parseEther("111.556"), // short token supply
+          parseEther("222.11245"), // long token supply
+          "ETH Gas Price", // reference asset
+          erc20.address, // collateral token
+          chainlinkOracle.address // data feed provider
+        ] 
+      ); 
+      await tx.wait();
+      const latestPoolId = await getterFacet.getLatestPoolId()
+      const poolParamsBefore = await getterFacet.getPoolParametersById(latestPoolId)
+      expect(poolParamsBefore.statusFinalReferenceValue).to.eq(0)
+      expect(poolParamsBefore.finalReferenceValue).to.eq(0)
+      
+      // Random user (here user1) sets final reference value
+      await chainlinkOracle.connect(user1).setFinalReferenceValue(divaKovanAddress, '36893488147419112854', latestPoolId) // roundId = '36893488147419112854' => price 412900500000 (4'129.005)
+
+      const poolParamsAfter = await getterFacet.getPoolParametersById(latestPoolId)
+      expect(poolParamsAfter.statusFinalReferenceValue).to.eq(3)
+      expect(poolParamsAfter.finalReferenceValue).to.eq(parseEther("17")) // 412900500000000000000000
+    })  
+
+    it('Should set final reference value equal to 4129.005 when triggered within the submission period', async () => {
+      let tx = await poolFacet.createContingentPool(
+        [
+          parseEther("17"), // inflection
+          parseEther("33"), // cap
+          parseEther("10"), // floor
+          parseEther("100"), // collateral balance short
+          parseEther("100"), // collateral balance long
+          getExpiryInSeconds(-100), // expiry date
+          parseEther("111.556"), // short token supply
+          parseEther("222.11245"), // long token supply
+          "ETH Gas Price", // reference asset
+          erc20.address, // collateral token
+          chainlinkOracle.address // data feed provider
+        ] 
+      ); 
+      await tx.wait();
+      const latestPoolId = await getterFacet.getLatestPoolId()
+      const poolParamsBefore = await getterFacet.getPoolParametersById(latestPoolId)
+      expect(poolParamsBefore.statusFinalReferenceValue).to.eq(0)
+      expect(poolParamsBefore.finalReferenceValue).to.eq(0)
+
+      // Random user (here user1) triggers setFinalReferenceValue
+      await chainlinkOracle.connect(user1).setFinalReferenceValue(divaKovanAddress, '36893488147419112854', latestPoolId) // roundId = '36893488147419112854' => price 412900500000 (4'129.005)
+
+      const poolParamsAfter = await getterFacet.getPoolParametersById(latestPoolId)
+      expect(poolParamsAfter.statusFinalReferenceValue).to.eq(3)
+      expect(poolParamsAfter.finalReferenceValue).to.eq(parseEther("4129.005")) 
+
+      // Another user (here user2) tries to trigger setFinalReferenceValue after the value has already been set
+      await expect(chainlinkOracle.connect(user2).setFinalReferenceValue(divaKovanAddress, '36893488147419112854', latestPoolId)).to.be.revertedWith("Settlement: Final reference value is already submitted/confirmed"); 
+    })   
+
+    it('Should reverts with message "Settlement: No permission to set the reference value" if a pool that has different data feed provider was provided', async () => {
+      let tx = await poolFacet.createContingentPool(
+        [
+          parseEther("17"), // inflection
+          parseEther("33"), // cap
+          parseEther("10"), // floor
+          parseEther("100"), // collateral balance short
+          parseEther("100"), // collateral balance long
+          getExpiryInSeconds(-100), // expiry date
+          parseEther("111.556"), // short token supply
+          parseEther("222.11245"), // long token supply
+          "ETH Gas Price", // reference asset
+          erc20.address, // collateral token
+          oracle.address // data feed provider
+        ] 
+      ); 
+      await tx.wait();
+      const latestPoolId = await getterFacet.getLatestPoolId()
+      const poolParamsBefore = await getterFacet.getPoolParametersById(latestPoolId)
+      expect(poolParamsBefore.statusFinalReferenceValue).to.eq(0)
+      expect(poolParamsBefore.finalReferenceValue).to.eq(0)
+      expect(poolParamsBefore.dataFeedProvider).to.not.eq(user1.address)
+
+      // Random user (here user1) triggers setFinalReferenceValue for a pool that has different data feed provider
+      await expect(chainlinkOracle.connect(user1).setFinalReferenceValue(divaKovanAddress, '36893488147419112854', latestPoolId)).to.be.revertedWith("Settlement: No permission to set the reference value") // roundId = '36893488147419112854' => price 412900500000 (4'129.005)
+
+    }) 
+
+    // TODO: Test where it's DIVA's turn to provide the value after submission period expired without any input
+  })
+  
+  
+  
+});
