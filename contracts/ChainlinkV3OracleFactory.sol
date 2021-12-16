6480a8ec569b71cad93703be8fb6dc9a7626d3e9
Walodja1987 <wladimir.weinbender@gmail.com>
2021-12-16 12:59:29 +0100

    interface for factoy contract added

--- contracts/ChainlinkV3OracleFactory.sol
+++ contracts/ChainlinkV3OracleFactory.sol
@@ -5,8 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "./interfaces/IChainlinkV3OracleFactory.sol";
 import "./ChainlinkV3Oracle.sol";
 
-// QUESTION: Add Ownable library?
-// TODO: Explore CloneFactory: https://soliditydeveloper.com/clonefactory
 /**
  * @notice Main contract to create DIVA specific Chainlink v3 oracles.
  * @dev Implementation of the {IChainlinkV3OracleFactory} interface.
@@ -16,7 +14,7 @@ contract ChainlinkV3OracleFactory is IChainlinkV3OracleFactory {
     address[] private _chainlinkV3Oracles;
     	
     // TODO: Use ENS name instead of address
-    function createChainlinkV3Oracle(address _chainlinkPriceFeed) public returns (ChainlinkV3Oracle chainlinkV3Oracle) {
+    function createChainlinkV3Oracle(address _chainlinkPriceFeed) external override {
         
         ChainlinkV3Oracle chainlinkV3Oracle = new ChainlinkV3Oracle(_chainlinkPriceFeed);
         _chainlinkV3Oracles.push(address(chainlinkV3Oracle));
@@ -24,15 +22,15 @@ contract ChainlinkV3OracleFactory is IChainlinkV3OracleFactory {
         emit OracleCreated(address(chainlinkV3Oracle), _chainlinkPriceFeed, msg.sender);
     }
 
-    function getChainlinkV3Oracles() external view returns (address[] memory) {
+    function getChainlinkV3Oracles() external view override returns (address[] memory) {
         return _chainlinkV3Oracles;
     }
 
-    function getChainlinkV3OraclesByIndex(uint256 _index) external view returns (address) {
+    function getChainlinkV3OraclesByIndex(uint256 _index) external view override returns (address) {
         return _chainlinkV3Oracles[_index];    
     }
 
-    function getChainlinkV3OraclesLength() external view returns (uint256) {
+    function getChainlinkV3OraclesLength() external view override returns (uint256) {
         return _chainlinkV3Oracles.length;
     }
 
--- contracts/interfaces/IChainlinkV3OracleFactory.sol
+++ contracts/interfaces/IChainlinkV3OracleFactory.sol
@@ -2,7 +2,31 @@
 pragma solidity 0.8.4;
 
 interface IChainlinkV3OracleFactory {
-    
+
+    /**
+     * @dev Deploys a DIVA specific Chainlink specific contract
+     * @param _chainlinkPriceFeed Chainlink price feed address
+     */
+    function createChainlinkV3Oracle(address _chainlinkPriceFeed) external;
+
+    /**
+     * @dev Returns the full array of deployed Chainlink oracles. 
+     * If array becomes too large, use {getChainlinkV3OraclesLength} and
+     * {getChainlinkV3OraclesByIndex} functions or leverage indexed data in
+     * TheGraph to retrieve the available oracle addresses
+     */
+    function getChainlinkV3Oracles() external view returns (address[] memory);
+
+    /**
+     * @dev Returns the Chainlink oracle address stored at array index `_index` 
+     */
+    function getChainlinkV3OraclesByIndex(uint256 _index) external view returns (address);
+
+    /**
+     * @dev Returns the length of the oracles array 
+     */
+    function getChainlinkV3OraclesLength() external view returns (uint256);
+
     /**
      * @notice Emitted when a new DIVA specific Chainlink oracle contract is deployed.
      * @param oracleAddress Address of the DIVA specific Chainlink oracle contract.
@@ -10,4 +34,5 @@ interface IChainlinkV3OracleFactory {
      * @param creator Address of the msg.sender.      
      */
     event OracleCreated(address indexed oracleAddress, address indexed chainlinkPriceFeed, address indexed creator);
+
 }
