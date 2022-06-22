76843d7530ae86e833496ccb6aff4ac94c8185fa
Rich Man <richman5225@gmail.com>
2022-06-22 11:28:59 -0400

    add DIVAOracleProtocol

---
+++ contracts/DIVAOracleProtocol.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./UsingTellor.sol";
+import "./interfaces/IDIVAOracleProtocol.sol";
+import "./interfaces/IBond.sol";
+import "./interfaces/IDIVA.sol";
+import "./libraries/SafeDecimalMath.sol";
+
+contract DIVAOracleProtocol is IDIVAOracleProtocol, Ownable, ReentrancyGuard {
+    using SafeDecimalMath for uint256;
+
+    // Storage
+    mapping(uint256 => bool) public poolIsSettled;
+
+    // Ordered to optimize storage
+    bool private immutable _challengeable;
+
+    constructor() {
+        _challengeable = false;
+    }
+
+    function setFinalReferenceValue(address _divaDiamond, uint256 _poolId)
+        external
+        override
+        nonReentrant
+    {
+        require(
+            !poolIsSettled[_poolId],
+            "DIVAOracleProtocol: pool is already settled"
+        );
+
+        IDIVA _diva = IDIVA(_divaDiamond);
+        IDIVA.Pool memory _params = _diva.getPoolParameters(_poolId); // updated the Pool struct based on the latest contracts
+
+        uint256 _expiryTime = _params.expiryTime;
+        require(
+            _expiryTime <= block.timestamp,
+            "DIVAOracleProtocol: Porter Bond is not expired yet"
+        );
+
+        string memory _porterBond = _params.referenceAsset;
+        IBond _bond = IBond(stringToAddress(_porterBond));
+
+        uint256 _amountUnpaid = _bond.amountUnpaid();
+
+        // Forward final value to DIVA contract. Allocates the fee as part of that process.
+        _diva.setFinalReferenceValue(_poolId, _amountUnpaid, _challengeable);
+
+        // Get the current fee claim allocated to this contract address (msg.sender)
+        uint256 feeClaim = _diva.getClaims(
+            _params.collateralToken,
+            address(this)
+        );
+
+        // Transfer fee claim to reporter and excessFeeRecipient
+        _diva.transferFeeClaim(msg.sender, _params.collateralToken, feeClaim);
+
+        // Set poolIsSettles as True
+        poolIsSettled[_poolId] = true;
+
+        emit FinalReferenceValueSet(_poolId, _amountUnpaid, _params.expiryTime);
+    }
+
+    function createContingentPool(
+        address _divaDiamond,
+        PorterPoolParams calldata _porterPoolParams
+    ) external override nonReentrant returns (uint256) {
+        address _porterBond = _porterPoolParams.referenceAsset;
+        IBond _bond = IBond(_porterBond);
+        uint256 gracePeriodEnd = _bond.gracePeriodEnd();
+
+        IDIVA.PoolParams memory _poolParams;
+        _poolParams.referenceAsset = addressToString(_porterBond);
+        _poolParams.expiryTime = uint96(gracePeriodEnd);
+        _poolParams.floor = _porterPoolParams.floor;
+        _poolParams.inflection = _porterPoolParams.inflection;
+        _poolParams.cap = _porterPoolParams.cap;
+        _poolParams.gradient = _porterPoolParams.gradient;
+        _poolParams.collateralAmount = _porterPoolParams.collateralAmount;
+        _poolParams.collateralToken = _porterPoolParams.collateralToken;
+        _poolParams.dataProvider = _porterPoolParams.dataProvider;
+        _poolParams.capacity = _porterPoolParams.capacity;
+
+        IDIVA _diva = IDIVA(_divaDiamond);
+        uint256 _poolId = _diva.createContingentPool(_poolParams);
+
+        emit PoolIssued(_poolId);
+
+        return _poolId;
+    }
+
+    function challengeable() external view override returns (bool) {
+        return _challengeable;
+    }
+
+    /**
+     * @notice Function to convert address to string.
+     */
+    function addressToString(address _addr)
+        public
+        pure
+        returns (string memory)
+    {
+        bytes32 _bytes = bytes32(uint256(uint160(_addr)));
+        bytes memory _hex = "0123456789abcdef";
+
+        bytes memory _string = new bytes(51);
+        _string[0] = "0";
+        _string[1] = "x";
+        for (uint256 i = 0; i < 20; i++) {
+            _string[2 + i * 2] = _hex[uint256(uint8(_bytes[i + 12] >> 4))];
+            _string[3 + i * 2] = _hex[uint256(uint8(_bytes[i + 12] & 0x0f))];
+        }
+        return string(_string);
+    }
+
+    /**
+     * @notice Function to convert string to address.
+     */
+    function stringToAddress(string memory _a) public pure returns (address) {
+        bytes memory tmp = bytes(_a);
+        uint160 iaddr = 0;
+        uint160 b1;
+        uint160 b2;
+        for (uint256 i = 2; i < 2 + 2 * 20; i += 2) {
+            iaddr *= 256;
+            b1 = uint160(uint8(tmp[i]));
+            b2 = uint160(uint8(tmp[i + 1]));
+            if ((b1 >= 97) && (b1 <= 102)) b1 -= 87;
+            else if ((b1 >= 48) && (b1 <= 57)) b1 -= 48;
+            if ((b2 >= 97) && (b2 <= 102)) b2 -= 87;
+            else if ((b2 >= 48) && (b2 <= 57)) b2 -= 48;
+            iaddr += (b1 * 16 + b2);
+        }
+        return address(iaddr);
+    }
+}
---
+++ contracts/interfaces/IDIVAOracleProtocol.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+interface IDIVAOracleProtocol {
+    // Argument for `createContingentPool` function
+    struct PorterPoolParams {
+        address referenceAsset;
+        uint256 floor;
+        uint256 inflection;
+        uint256 cap;
+        uint256 gradient;
+        uint256 collateralAmount;
+        address collateralToken;
+        address dataProvider;
+        uint256 capacity;
+    }
+
+    /**
+     * @notice Emitted when the final reference value is set.
+     * @param poolId The Id of an existing derivatives pool
+     * @param finalValue Unpaid amount from porter bond
+     * @param expiryTime Unix timestamp in seconds of pool expiry date
+     */
+    event FinalReferenceValueSet(
+        uint256 indexed poolId,
+        uint256 finalValue,
+        uint256 expiryTime
+    );
+
+    /**
+     * @notice Emitted when a new pool is created.
+     * @param poolId The Id of the newly created contingent pool.
+     */
+    event PoolIssued(uint256 indexed poolId);
+
+    /**
+     * @dev Function to set the final reference value for a given `poolId`.
+     * @param _divaDiamond Address of the diva smart contract. Used as argument
+     * rather than a hard-coded constant to avoid redeploying the oracle contracts
+     * when a new version of DIVA Protocol is released.
+     * @param _poolId The unique identifier of the pool.
+     */
+    function setFinalReferenceValue(address _divaDiamond, uint256 _poolId)
+        external;
+
+    /**
+     * @notice Function to create contingent pool
+     * @dev Position token supply equals `collateralAmount` (minimum 1e6).
+     * Position tokens have the same amount of decimals as the collateral token.
+     * Only ERC20 tokens with 6 <= decimals <= 18 are accepted as collateral.
+     * Tokens with flexible supply like Ampleforth should not be used. When
+     * interest/yield bearing tokens are considered, only use tokens with a
+     * constant balance mechanism such as Compound's cToken or the wrapped
+     * version of Lido's staked ETH (wstETH).
+     * ETH is not supported as collateral in v1. It has to be wrapped into WETH
+       before deposit.
+     * @param _divaDiamond Address of the diva smart contract. Used as argument
+     * rather than a hard-coded constant to avoid redeploying the oracle contracts
+     * when a new version of DIVA Protocol is released.
+     * @param _porterPoolParams Struct containing the pool specification:
+     * - referenceAsset: The address of bond contract.
+     * - floor: Value of underlying at or below which the short token will pay
+         out the max amount and the long token zero.
+     * - inflection: Value of underlying at which the long token will payout
+         out `gradient` and the short token `1-gradient`.
+     * - cap: Value of underlying at or above which the long token will pay
+         out the max amount and short token zero.
+     * - gradient: Long token payout at inflection. The short token payout at
+         inflection is `1-gradient`.
+     * - collateralAmount: Collateral amount to be deposited into the pool to
+         back the position tokens.
+     * - collateralToken: ERC20 collateral token address.
+     * - dataProvider: Address that is supposed to report the final value of
+         the reference asset.
+     * - capacity: The maximum collateral amount that the pool can accept.
+     * @return poolId
+     */
+    function createContingentPool(
+        address _divaDiamond,
+        PorterPoolParams calldata _porterPoolParams
+    ) external returns (uint256);
+
+    /**
+     * @dev Returns whether the oracle's data feed is challengeable or not.
+     * Will return false in that implementation.
+     */
+    function challengeable() external view returns (bool);
+}
