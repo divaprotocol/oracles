7641ada31cc569b79e9065f3b4378ed519bbbeaf
Walodja1987 <wladimir.weinbender@gmail.com>
2022-08-25 00:13:08 +0200

    etherscan verification script added for DIVAOracleTellor

---
+++ deploy-args.js
@@ -0,0 +1,7 @@
+// Input the arguments for the constructor
+module.exports = [
+    "0x7B8AC044ebce66aCdF14197E8De38C1Cc802dB4A", // TellorPlayground address
+    "0x1EE5730C710cF06dFA7952D61A321eC8e16b9d3A", // excessFeeRecipient
+    10, // periodMinPeriodUndisputed in seconds
+    "10000000000000000000" // maxFeeAmountUSD expressed as an integer with 18 decimals
+  ];
